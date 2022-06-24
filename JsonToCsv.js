b7460ae949f17acc53d4fe4c544ac63389f4b640
Vinicius Bernardes Silva <vinicius.bsilva@hotmail.com>
2022-06-24 02:51:35 -0300

    Transform JSON to CSV logic copied to main program and simplified. Pending to adapt download logic

--- JsonToCsv.js
+++ JsonToCsv.js
@@ -83,17 +83,55 @@ function download() {
         .then(response => response.json())
         .then(json => console.log(convertToCSV(json)));
 
+}
 
+function convertToCSV(json) {
+
+    // Create an array from the JSON string (or just use it if it's already an array)
+    const jsonArray = Array.isArray(json) ? json : [json];
+
+    // Iterate recursively through the keys of the first line of the JSON to get nested keys and use all as header
+    const jsonKeys = readJson(jsonArray[0], true);
+
+    // Iterate recursively through the JSON elements to get nested values
+    const jsonValues = [];
+    jsonArray.forEach(item => {
+        jsonValues.push(readJson(item))
+    });
+
+    // Concat results
+    const resultArray = [].concat([jsonKeys], jsonValues);
+
+    // Return results with each line (item) separated by "\n"
+    return resultArray.join("\n");
 
 }
 
-function convertToCSV(arr) {
-    const keys = Object.keys(arr[0]);
-    console.log("keys", keys);
+/* 
+* Parameter readKeys ->  toogle read keys / read values 
+* false = read ONLY values  (DEFAULT) 
+* true = read ONLY keys
+*/
+function readJson(object, readKeys = false) {
 
-    const array = [keys].concat(arr)
+    const elements = [];
 
-    return array.map(it => {
-        return Object.values(it).toString()
-    }).join('\n')
+    // iterate through every property
+    for (property in object) {
+
+        /* If value is an object it means it's a nested property.
+         *  In that case this function must be called again in order to 
+         *  iterate through the nested properties and get back
+         */
+        if (typeof object[property] === "object") {
+            // spread so this function won't return nested arrays
+            elements.push(...readJson(object[property]));
+        } else {
+            readKeys ? elements.push(property) : elements.push(object[property]);
+        }
+
+    }
+
+    return elements;
+
 }
